45927c7ce2cf043e1a8a6e873dcf3cbb1b1ca686
Tatsuya Hanyu <s1210099@gmail.com>
2018-05-29 23:26:37 +0900

    [ADD] cross validation experiments function. Experiments for one camera movie

--- estimate_pose.py
+++ estimate_pose.py
@@ -1,8 +1,12 @@
 import cv2
 import numpy as np
 from sklearn import svm
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import ShuffleSplit
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics.classification import cohen_kappa_score
+from sklearn import neural_network
 
 class EstimatePoseMovie:
     def __init__(self):
@@ -16,30 +20,42 @@ class EstimatePoseMovie:
 
         ret, frame = src_video.read()
         while ret is True:
+            frame = np.reshape(frame, (921600,))
             feature.append(frame)
             ret, frame = src_video.read()
 
         self.feature[label] = feature
 
-    def training(self, use_feature, cross_validation):
+    def cross_validation(self, use_feature, cross_validation):
         self.train_data = []
         self.label_data = []
 
-
-        for label in use_feature:
-            self.label_data.extend([label for x in self.feature[label]])
+        for x, label in enumerate(use_feature):
+            self.label_data.extend([x for i in self.feature[label]])
             self.train_data.extend(self.feature[label])
 
-        print("shape", self.train_data)
+        print("shape", np.shape(self.train_data))
+        print("shape", np.shape(self.label_data))
         print("shape", self.label_data)
-
-        clf = svm.SVC(kernel='linear', C=1)
-        scores = cross_val_score(clf, self.train_data, self.label_data, cv=cross_validation)
-        print("scores", scores)
+        #kernel='rbf', C=1
+        clf = svm.SVC(kernel='rbf', C=1)
+        # clf = neural_network.MLPClassifier(activation="relu")
+        # cv = ShuffleSplit(n_splits=cross_validation, test_size=0.3, random_state=0)
+        # print(cv)
+        y_pred = cross_val_predict(clf, self.train_data, self.label_data, cv=cross_validation)
+        accuracy = cohen_kappa_score(self.label_data, y_pred)
+        conf_mat = confusion_matrix(self.label_data, y_pred)
+        print("accuracy", accuracy)
+        print("conf_mat", conf_mat)
 
 
 if __name__ == "__main__":
     estimator = EstimatePoseMovie()
-    estimator.mk_feature("sit", "mp4")
+    estimator.mk_feature("sit", "../experiments_data/bone_picture/sit_0.mp4")
+    estimator.mk_feature("t_pose", "../experiments_data/bone_picture/t_pose_0.mp4")
+    estimator.mk_feature("raise_hands", "../experiments_data/bone_picture/raise_hands_re_0.mp4")
+    estimator.mk_feature("walking", "../experiments_data/bone_picture/background_walking_bone_0.mp4")
+    estimator.cross_validation(use_feature=["sit", "t_pose", "raise_hands", "walking"], cross_validation=3)
 
-    estimator.training(use_feature=["sit", "raise_hands"], cross_validation=3)
+    second_person = EstimatePoseMovie()
+
