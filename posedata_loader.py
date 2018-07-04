0c7712a1b154f2ba855c73d8f9182aac42ebd762
Tatsuya Hanyu <s1210099@gmail.com>
2018-07-04 09:36:11 +0900

    [fix] label の設計

--- posedata_loader.py
+++ posedata_loader.py
@@ -37,8 +37,9 @@ class PosedataLoader:
             print("label array shape", np.shape(self.label_data[i]))
             label_arr.extend(self.label_data[i])
 
-        use_feature = [feature_arr[i] for i, label in enumerate(label_arr) if label > 0 or label == 1]
-        use_label = [i for i in label_arr if i > 0 or i == 1]
+        use_feature = [feature_arr[i] for i, label in enumerate(label_arr) if label > 0 and label != 1]
+        use_label_ = [i for i in label_arr if i > 0 and i != 1]
+        use_label = [i-1 for i in use_label_ if i == 2]
         use_label, use_feature =\
             self.mk_movie_data.mk_feature_humanextraction_array(
                 labelarray=use_label,
